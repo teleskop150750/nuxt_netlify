dbcb27f6b69cc6ffb8122bb3faa2737f884c760a
teleskop150750 <teleskop150750@gmail.com>
2024-12-03 00:21:46 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <NuxtLink to="/"> Home </NuxtLink>
+    <NuxtLink to="/reviews/"> Reviews </NuxtLink>
+
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { data } = await useAsyncData(
+  'home',
+  async () => {
+    const response = await $fetch('https://jsonplaceholder.typicode.com/todos');
+
+    return response;
+  },
+  { deep: false }
+);
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+
+    <pre>{{ data }}</pre>
+  </div>
+</template>
