44a42a88f1a367d5c137958871db653a5c9a3a4f
teleskop150750 <teleskop150750@gmail.com>
2024-12-03 00:37:49 +0300

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  debug: true,
+  sourcemap: true,
+  vite: {
+    build: {
+      minify: false,
+    },
+  },
 });
